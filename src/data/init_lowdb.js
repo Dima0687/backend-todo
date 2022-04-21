f916805fcff0cac884fff6f0d42a9460749db4b3
Dietmar <eis0687.dietmar@gmail.com>
2022-04-21 13:47:52 +0200

    init_lowdb ready

--- src/data/init_lowdb.js
+++ src/data/init_lowdb.js
@@ -4,4 +4,11 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const file = join(__)
+const file = join(__dirname, 'db.json');
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+
+await db.read();
+
+db.data ||= { todos: [] }
+
